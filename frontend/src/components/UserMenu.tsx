3454e7662ed6c273628b754c3199c2ca39acc2ce
bence <benceluzsinszky@gmail.com>
2024-05-30 20:13:20 +0200

    first working version of update methods

--- frontend/src/components/UserMenu.tsx
+++ frontend/src/components/UserMenu.tsx
@@ -1,13 +1,21 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { IsLoggedInContext } from "../GlobalContext";
 
 interface UserMenuProps {
     setShowUserMenu: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
+interface UpdatedUser {
+    username?: string;
+    password?: string;
+}
+
 
 export default function UserMenu({ setShowUserMenu }: UserMenuProps) {
-    const { setIsLoggedIn, loggedInUser } = useContext(IsLoggedInContext);
+    const { setIsLoggedIn, loggedInUser, setLoggedInUser } = useContext(IsLoggedInContext);
+
+    const [isEditingUsername, setIsEditingUsername] = useState(false);
+    const [isEditingPassword, setIsEditingPassword] = useState(false);
 
     const handleLogOut = () => {
         localStorage.removeItem('token');
@@ -16,20 +24,109 @@ export default function UserMenu({ setShowUserMenu }: UserMenuProps) {
 
 
 
+    const handleSubmit = () => {
+        const passwordElement = document.querySelector('.current-password-input') as HTMLInputElement;
+        const password = passwordElement ? passwordElement.value : '';
+
+        const updatedUser: UpdatedUser = {};
+
+        if (isEditingUsername) {
+            const usernameElement = document.querySelector('.username-input') as HTMLInputElement;
+            updatedUser.username = usernameElement.value;
+        }
+
+        if (isEditingPassword) {
+            const newPassword1Element = document.querySelector('.new-password-1') as HTMLInputElement;
+            const newPassword2Element = document.querySelector('.new-password-2') as HTMLInputElement;
+            if (newPassword1Element.value !== newPassword2Element.value) {
+                alert('Passwords do not match');
+                return;
+            }
+            updatedUser.password = newPassword1Element.value;
+        }
+
+        fetch('http://localhost:8000/token', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: new URLSearchParams({
+                username: loggedInUser || '',
+                password: password
+            })
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(response.statusText);
+                }
+                return response.json();
+            })
+            .then(data => {
+                const token = data.access_token;
+                console.log(token);
+                localStorage.setItem('token', token);
+                return fetch('http://localhost:8000/users/me', {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${data.access_token}`
+                    },
+                    body: JSON.stringify(updatedUser)
+                }).then(response => {
+                    if (response.ok) {
+                        if (updatedUser.username) {
+                            setLoggedInUser(updatedUser.username);
+                        }
+                        setIsEditingPassword(false);
+                        setIsEditingUsername(false);
+                        alert('Success!');
+                        return;
+                    }
+                }).catch(error => {
+                    alert(error);
+                    return;
+                });
+            })
+            .catch(error => {
+                alert(error);
+                return;
+            });
+    };
+
+
     return (
         <div className="user-menu">
             <div className="close-user-menu">
-                <button onClick={() => setShowUserMenu(false)}>X</button>
+                <button onClick={() => setShowUserMenu(false)}><img src="../../public/close.png" alt="Close button" /></button>
             </div>
-            <div className="change-user-name">
-                <p>Username</p>
-                <p>{loggedInUser}</p>
+            <div className="change-user-details">
+                <div className="change-username">
+                    {isEditingUsername ? (
+                        <input
+                            type="text"
+                            placeholder="Username"
+                            value={loggedInUser || ''}
+                            className="username-input"
+                        />
+                    ) : (
+                        <p className="username">{loggedInUser}</p>
+                    )}
+                    <button onClick={() => { setIsEditingUsername(!isEditingUsername) }}>Change</button>
+                </div>
+                <div className="change-password">
+                    <div className="current-password">
+                        <input type="password" placeholder="Password" className="current-password-input"></input>
+                        <button onClick={() => { setIsEditingPassword(!isEditingPassword) }}>Change</button>
+                    </div>
+                    <div className="new-password">
+                        {isEditingPassword && <input type="password" placeholder="New password" className="new-password-1"></input>}
+                        {isEditingPassword && <input type="password" placeholder="New password again" className="new-password-2"></input>}
+                    </div>
+                </div>
             </div>
-            <div className="change-password">
-                <p>Password</p>
-            </div>
-            <div className="log-out">
+            <div className="logout">
                 <button onClick={() => handleLogOut()}>Log out</button>
+                <button onClick={() => handleSubmit()}>Submit</button>
             </div>
         </div>
     )
